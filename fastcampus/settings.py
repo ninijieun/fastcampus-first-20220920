c8fba155639374f56a23e7d8b2dbdbf2d3eb46c3
ninijieun <qkrwldms200@gmail.com>
2022-09-20 14:55:21 +0900

    AWS 데이터베이스 연동 실패로 주석처리

--- fastcampus/settings.py
+++ fastcampus/settings.py
@@ -77,19 +77,19 @@ WSGI_APPLICATION = 'fastcampus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'project1',
-        'USER': 'admin',
-        'PASSWORD':'adminadmin',
-        'HOST' : 'database-1.cdtdptjnqjgh.ap-northeast-2.rds.amazonaws.com',
-        'PORT':'3306',
-        'OPTIONS':{
-            'init_command':"SET sql_mode = 'STRICT_TRANS_TABLES'"
-        }
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'project1',
+#         'USER': 'admin',
+#         'PASSWORD':'adminadmin',
+#         'HOST' : 'database-1.cdtdptjnqjgh.ap-northeast-2.rds.amazonaws.com',
+#         'PORT':'3306',
+#         'OPTIONS':{
+#             'init_command':"SET sql_mode = 'STRICT_TRANS_TABLES'"
+#         }
+#     }
+# }
 #데이터베이스 이름 : project1
 # 비밀번호 : adminadmin
 
