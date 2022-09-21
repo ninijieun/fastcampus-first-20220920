2119d34c0ecbca433d712c98909b7d5d55a2f587
ninijieun <qkrwldms200@gmail.com>
2022-09-21 16:03:11 +0900

    freeze

--- fastcampus/settings.py
+++ fastcampus/settings.py
@@ -76,17 +76,22 @@ WSGI_APPLICATION = 'fastcampus.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+DATABASES = {
+    'default' : {
+        'ENGINE' : 'django.db.backends.sqlite3',
+        'NAME'  : BASE_DIR / 'db.sqlite3',
+    }
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
 #         'NAME': 'project1',
-#         'USER': 'admin',
+#         'USER' : 'admin',
 #         'PASSWORD':'adminadmin',
-#         'HOST' : 'database-1.cdtdptjnqjgh.ap-northeast-2.rds.amazonaws.com',
+#         'HOST':'database-1.cdtdptjnqjgh.ap-northeast-2.rds.amazonaws.com',
 #         'PORT':'3306',
 #         'OPTIONS':{
-#             'init_command':"SET sql_mode = 'STRICT_TRANS_TABLES'"
+#             'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
 #         }
 #     }
 # }
